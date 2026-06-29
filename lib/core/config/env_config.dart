e3b4da76353a66da81379eeb3b298d479f2143b8
Panha <panha@Panhas-MacBook-Air.local>
2026-06-29 16:45:44 +0700

    chore: save Supabase credentials to env_config

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -22,6 +22,6 @@ class EnvConfig {
   // ==========================================
   // SUPABASE CONFIGURATIONS
   // ==========================================
-  static const String supabaseUrl = ''; // Enter your Supabase project URL here
-  static const String supabaseAnonKey = ''; // Enter your Supabase Anon Key here
+  static const String supabaseUrl = 'https://kuksgolxnnnvjkiwvuzr.supabase.co'; // Enter your Supabase project URL here
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1a3Nnb2x4bm5udmpraXd2dXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTEyOTYsImV4cCI6MjA5NzU4NzI5Nn0.F0I17NzcChiUfJbIs_YiBmkLvKAYUI67C49_fZpvZvg'; // Enter your Supabase Anon Key here
 }
